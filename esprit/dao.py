d27799bebf76a87eb1402aed4abb3298dde8590c
Richard Jones <richard@cottagelabs.com>
2015-06-23 15:17:26 +0100

    prep dao for supporting dynamic read/write index types

--- esprit/dao.py
+++ esprit/dao.py
@@ -47,7 +47,37 @@ class DomainObject(DAO):
     
     def __init__(self, raw=None):
         self.data = raw if raw is not None else {}
-    
+
+    @classmethod
+    def dynamic_read_types(cls):
+        return None
+
+    @classmethod
+    def dynamic_write_type(cls):
+        return None
+
+    @classmethod
+    def get_read_types(cls, types=None):
+        if types is not None:
+            if not isinstance(types, list):
+                return [types]
+            return types
+        drt = cls.dynamic_read_types()
+        if drt is not None:
+            if not isinstance(drt, list):
+                return [drt]
+            return drt
+        return [cls.__type__]
+
+    @classmethod
+    def get_write_type(cls, type=None):
+        if type is not None:
+            return type
+        dwt = cls.dynamic_write_type()
+        if dwt is not None:
+            return dwt
+        return cls.__type__
+
     @property
     def id(self):
         return self.data.get('id', None)
@@ -83,29 +113,33 @@ class DomainObject(DAO):
         raw.refresh(conn)
     
     @classmethod
-    def pull(cls, id_, conn=None, wrap=True):
+    def pull(cls, id_, conn=None, wrap=True, types=None):
         '''Retrieve object by id.'''
         if conn is None:
             conn = cls.__conn__
-        
+
+        types = cls.get_read_types(types)
+
         if id_ is None:
             return None
         try:
-            resp = raw.get(conn, cls.__type__, id_)
-            if resp.status_code == 404:
-                return None
-            else:
-                j = raw.unpack_get(resp)
-                if wrap:
-                    return cls(j)
+            for t in types:
+                resp = raw.get(conn, t, id_)
+                if resp.status_code == 404:
+                    continue
                 else:
-                    return j
+                    j = raw.unpack_get(resp)
+                    if wrap:
+                        return cls(j)
+                    else:
+                        return j
+            return None
         except Exception as e:
             print e.message
             return None
     
     @classmethod
-    def query(cls, q='', terms=None, should_terms=None, facets=None, conn=None, **kwargs):
+    def query(cls, q='', terms=None, should_terms=None, facets=None, conn=None, types=None, **kwargs):
         '''Perform a query on backend.
 
         :param q: maps to query_string parameter if string, or query dict if dict.
@@ -116,6 +150,8 @@ class DomainObject(DAO):
         '''
         if conn is None:
             conn = cls.__conn__
+
+        types = cls.get_read_types(types)
         
         if isinstance(q,dict):
             query = q
@@ -177,16 +213,21 @@ class DomainObject(DAO):
                 if not isinstance(should_terms[s],list): should_terms[s] = [should_terms[s]]
                 query["query"]["bool"]["must"].append({"terms" : {s : should_terms[s]}})
 
-        r = raw.search(conn, cls.__type__, query)
+        r = raw.search(conn, types, query)
         return r.json()
 
     @classmethod
-    def object_query(cls, q='', terms=None, should_terms=None, facets=None, conn=None, **kwargs):
-        j = cls.query(q=q, terms=terms, should_terms=should_terms, facets=facets, conn=conn, **kwargs)
+    def object_query(cls, q='', terms=None, should_terms=None, facets=None, conn=None, types=None, **kwargs):
+        j = cls.query(q=q, terms=terms, should_terms=should_terms, facets=facets, conn=conn, types=types, **kwargs)
         res = raw.unpack_json_result(j)
         return [cls(r) for r in res]
 
-    def save(self, conn=None, makeid=True, created=True, updated=True, blocking=False):
+    def save(self, conn=None, makeid=True, created=True, updated=True, blocking=False, type=None):
+        if conn is None:
+            conn = self.__conn__
+
+        type = self.get_write_type(type)
+
         if blocking and not updated:
             raise StoreException("Unable to do blocking save on record where last_updated is not set")
 
@@ -206,10 +247,8 @@ class DomainObject(DAO):
                 self.data['created_date'] = now
         if updated:
             self.data['last_updated'] = now
-        
-        if conn is None:
-            conn = self.__conn__
-        raw.store(conn, self.__type__, self.data, self.id)
+
+        raw.store(conn, type, self.data, self.id)
 
         if blocking:
             q = {
@@ -219,7 +258,7 @@ class DomainObject(DAO):
                 "fields" : ["last_updated"]
             }
             while True:
-                res = raw.search(conn, self.__type__, q)
+                res = raw.search(conn, type, q)
                 j = raw.unpack_result(res)
                 if len(j) == 0:
                     time.sleep(0.5)
@@ -232,19 +271,23 @@ class DomainObject(DAO):
                     time.sleep(0.5)
                     continue
         
-    def delete(self, conn=None):
+    def delete(self, conn=None, type=None):
         if conn is None:
             conn = self.__conn__
-        raw.delete(conn, self.__type__, self.id)
+        type = self.get_write_type(type)
+
+        raw.delete(conn, type, self.id)
 
     @classmethod
-    def delete_by_query(cls, query, conn=None, es_version="0.90.13"):
+    def delete_by_query(cls, query, conn=None, es_version="0.90.13", type=None):
         if conn is None:
             conn = cls.__conn__
-        raw.delete_by_query(conn, cls.__type__, query, es_version=es_version)
+        type = cls.get_write_type(type)
+
+        raw.delete_by_query(conn, type, query, es_version=es_version)
 
     @classmethod
-    def iterate(cls, q, page_size=1000, limit=None, wrap=True):
+    def iterate(cls, q, page_size=1000, limit=None, wrap=True, **kwargs):
         q = q.copy()
         q["size"] = page_size
         q["from"] = 0
@@ -256,7 +299,7 @@ class DomainObject(DAO):
             if limit is not None and counter >= limit:
                 break
 
-            res = cls.query(q=q)
+            res = cls.query(q=q, **kwargs)
             rs = [r.get("_source") if "_source" in r else r.get("fields") for r in res.get("hits", {}).get("hits", [])]
             # print counter, len(rs), res.get("hits", {}).get("total"), len(res.get("hits", {}).get("hits", [])), json.dumps(q)
             if len(rs) == 0:
@@ -273,26 +316,26 @@ class DomainObject(DAO):
             q["from"] += page_size
 
     @classmethod
-    def iterall(cls, page_size=1000, limit=None):
-        return cls.iterate(deepcopy(all_query), page_size, limit)
+    def iterall(cls, page_size=1000, limit=None, **kwargs):
+        return cls.iterate(deepcopy(all_query), page_size, limit, **kwargs)
 
     @classmethod
-    def count(cls, q):
+    def count(cls, q, **kwargs):
         q = deepcopy(q)
         q["size"] = 0
-        res = cls.query(q=q)
+        res = cls.query(q=q, **kwargs)
         return res.get("hits", {}).get("total")
 
     @classmethod
-    def scroll(cls, q=None, page_size=1000, limit=None, keepalive="1m", conn=None, raise_on_scroll_error=True):
+    def scroll(cls, q=None, page_size=1000, limit=None, keepalive="1m", conn=None, raise_on_scroll_error=True, types=None):
         if conn is None:
             conn = cls.__conn__
+        types = cls.get_read_types(types)
 
         if q is None:
             q = {"query" : {"match_all" : {}}}
 
-
-        gen = tasks.scroll(conn, cls.__type__, q, page_size=page_size, limit=limit, keepalive=keepalive)
+        gen = tasks.scroll(conn, types, q, page_size=page_size, limit=limit, keepalive=keepalive)
 
         try:
             for o in gen:
